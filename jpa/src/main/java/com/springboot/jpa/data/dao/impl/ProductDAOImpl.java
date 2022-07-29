a00a49bebcc6b86243af6fb55d281d6bf280b0b9
조대준 <eowns36@gmail.com>
2022-07-29 21:03:53 +0900

    commit

--- jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
+++ jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.springboot.jpa.data.repository.ProductRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Component
 public class ProductDAOImpl implements ProductDAO {
 
@@ -19,21 +22,45 @@ public class ProductDAOImpl implements ProductDAO {
 
     @Override
     public Product insertProduct(Product product) {
-        return null;
+        Product savedProduct = productRepository.save(product);
+        return savedProduct;
     }
 
     @Override
     public Product selectProduct(Long number) {
-        return null;
+        Product selectProduct = productRepository.getById(number);
+        return selectProduct;
     }
 
     @Override
     public Product updateProductName(Long number, String name) throws Exception {
-        return null;
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+        if(selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdatedAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+
+        return updatedProduct;
     }
 
     @Override
     public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
 
+        if(selectedProduct.isPresent()){
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
     }
 }
